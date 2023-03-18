877ad7b24a171039d48ae2796b027c1471df9eb0
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-03-18 11:35:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+
+function findMatching(array, string){
+    return array.filter(function(element){
+        let allcaps = element.toUpperCase();
+        if (allcaps == string.toUpperCase()){
+            return element;
+        }
+    });
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(function(element){
+        return (element.startsWith(string));
+    });
+}
+
+function matchName(array, string){
+    return array.filter(function(element){
+        return element.name == string;
+    })
+}
